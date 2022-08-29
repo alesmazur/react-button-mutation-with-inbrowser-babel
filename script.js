a361f8dbb78d2bc35361465c609bcef8d80ec260
Ales Mazur <alesmazurphotography@gmail.com>
2022-08-29 14:42:05 +0200

    added more explanation comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// in Apps function parrameter I put variable that was made by object destructurisation and this object is props of App
 const App = ({ initialButtonText }) => {
   // object destructurisation by useState, or we can say that here we create states for App functional component;  method of React object for creation new vars: buttonText and setButtonText, to use them later in arrow functions for second "App" function calling and since that changing real-DOM by "App" function JSX return
 
